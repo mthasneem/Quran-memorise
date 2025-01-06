9e9cb3c19df0fecf25490efffa038d4a4fa479fd
Mohamed Thasneem <s19003377@ousl.lk>
2025-01-06 22:33:35 +0530

    libraries set in main file

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from quran_memorization_app import QuranMemorizationApp
-
+import tk
+import tkinter
+import customtkinter
+import requests
 if __name__ == "__main__":
     app = QuranMemorizationApp()
     app.mainloop()
